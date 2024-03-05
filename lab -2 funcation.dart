8af22ed8b76fd7a2d165e58eede353c1b51bd716
Obsu Kebede <134233644+ob971@users.noreply.github.com>
2024-03-05 15:36:32 +0300

    Add files via upload

---
+++ lab -2 funcation.dart
@@ -0,0 +1,16 @@
+double calculateAverage(List<num> numbers) {
+  if (numbers.isEmpty) {
+    return 0;
+  }
+  double sum = 0;
+  for (var number in numbers) {
+    sum += number;
+  }
+  return sum / numbers.length;
+}
+
+void main() {
+  List<double> numbersList = [5, 15, 25, 35, 45];
+  double average = calculateAverage(numbersList);
+  print("Average: $average");
+}
