1630ea1b8569444013ffbdb1a1638611fcf33736
Obsu Kebede <134233644+ob971@users.noreply.github.com>
2024-03-05 16:02:01 +0300

    Add files via upload

---
+++ lab 3 Asynchronous Programming.dart
@@ -0,0 +1,10 @@
+Future<int> fetchNumber() {
+  return Future.delayed(Duration(seconds: 2), () => 42);
+}
+
+void main() {
+  print('Fetching number...');
+  fetchNumber().then((result) {
+    print('Received number: $result');
+  });
+}
