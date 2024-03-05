02b3b26cd5035449628e3a521faa2cf71ec0dd2f
Obsu Kebede <134233644+ob971@users.noreply.github.com>
2024-03-05 15:25:08 +0300

    Update LAB-1-Control Flow Statements.dart

--- LAB-1-Control Flow Statements.dart
+++ LAB-1-Control Flow Statements.dart
@@ -1,1 +1,32 @@
-LAB-1-Control Flow Statements.dart
+
+void main() {
+  int day = 3; // Change the value of day to test different cases
+  
+  switch (day) {
+    case 1:
+      print('Sunday');
+      break;
+    case 2:
+      print('Monday');
+      break;
+    case 3:
+      print('Tuesday');
+      break;
+    case 4:
+      print('Wednesday');
+      break;
+    case 5:
+      print('Thursday');
+      break;
+    case 6:
+      print('Friday');
+      break;
+    case 7:
+      print('Saturday');
+      break;
+    default:
+      print('Invalid day');
+  }
+}
+
+
