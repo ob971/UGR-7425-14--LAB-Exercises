8f91ca7604898c8bd9714d0ae1c24587fa5cfe3d
Obsu Kebede <134233644+ob971@users.noreply.github.com>
2024-03-05 15:22:05 +0300

    Update LAB-1-Variables-and-types.dart

--- LAB-1-Variables-and-types.dart
+++ LAB-1-Variables-and-types.dart
@@ -1,1 +1,10 @@
-LAB-1-Variables-and-types.dart
+
+void main() {
+  // Declare three variables, a, b, and c, and assign them integer values
+  int a = 5;
+  int b = 10;
+  int c = 7;
+
+  // Print the sum of the variables
+  print('The sum of $a, $b, and $c is: ${a + b + c}');
+}
