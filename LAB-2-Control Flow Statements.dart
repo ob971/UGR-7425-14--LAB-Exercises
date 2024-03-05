e625043d70ab40e5d7fd9f1cb4f8098b58885823
Obsu Kebede <134233644+ob971@users.noreply.github.com>
2024-03-05 15:30:16 +0300

    Add files via upload

---
+++ LAB-2-Control Flow Statements.dart
@@ -0,0 +1,14 @@
+void main() {
+  int n = 10;
+  int first = 0;
+  int second = 1;
+  
+  print('First $n numbers in the Fibonacci sequence:');
+  
+  for (int i = 0; i < n; i++) {
+    print(first);
+    int next = first + second;
+    first = second;
+    second = next;
+  }
+}
